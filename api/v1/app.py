51b9ca74da0020bdfb9573929035aa7992e9c3fc
Hillary Cheserek <cheserekhillary@gmail.com>
2023-10-31 01:27:39 -0700

    doing the documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,84 +1,50 @@
 #!/usr/bin/python3
-
-"""
-Flask Application
-
-This script defines a Flask application for the AirBnB clone Restful API. It configures the application, 
-defines error handlers, and sets up CORS.
-
-"""
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
+
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={'/*': {'origins': app_host}})
+
 
 @app.teardown_appcontext
-def close_db(error):
-    """
-    Close Storage
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
+    storage.close()
 
-    This function is a callback that Flask will call when the application context is being destroyed. 
-    It is responsible for closing the database storage.
-
-    Args:
-        error: An optional error object.
-
-    Returns:
-        None
-    """
-
-storage.close()
 
 @app.errorhandler(404)
-def not_found(error):
-    """
-    404 Error
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
-    This function is an error handler for HTTP 404 (Not Found) errors. It returns a JSON response 
-    with an error message when a resource is not found.
 
-    Returns:
-        JSON response with a 404 error message.
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
 
-Swagger(app)
-
-if __name__ == "__main__":
-    """
-    Main Function
-
-    This is the main entry point of the script. It configures and starts the Flask application.
-
-    The following environment variables can be used to configure the application:
-    - HBNB_API_HOST: Host for the application. Defaults to '0.0.0.0'.
-    - HBNB_API_PORT: Port for the application. Defaults to '5000'.
-
-    Returns:
-        None
-    """
-
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-
-    app.run(host=host, port=port, threaded=True)
-
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
