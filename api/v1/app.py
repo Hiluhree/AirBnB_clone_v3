96b62fe38a503c382589cce72fa40c72ddf592a8
Hillary Cheserek <cheserekhillary@gmail.com>
2023-10-31 00:25:04 -0700

    documenting the file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
 from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 
+# Global Flask Application Variable: app
 app = Flask(__name__)
+
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
+
+# Cross-Origin Resource Sharing
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+# begin flask page rendering
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
@@ -18,7 +27,8 @@ def close_db(error):
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
+    """
+    handles 404 errors, in the event that global error handler fails
     ---
     responses:
       404:
